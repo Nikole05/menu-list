4d7ecdeeb57a40cf3ff3a4c86f3d0f1f493895c4
Nico <nico@Nicos-MacBook-Pro.local>
2018-11-26 19:22:47 +0200

    using styled-components in MealCard page

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -1,19 +1,35 @@
 import React, { Component } from "react";
+import styled from 'styled-components';
 import Title from "./Title";
 import Image from "./Image";
+
 export default class MealCard extends Component {
   render() {
+      console.log("PROPS=>", this.props);
     return (
       <div>
         {this.props.meals.map((item, index) => {
           return (
-            <a key={index} href={item.strSource}>
+            <Card 
+            key={index} 
+            href={item.strSource}>
               <Image source={item.strMealThumb} text={item.strMeal} />
               <Title title={item.strMeal} />
-            </a>
-          );
+            </Card>
+          )
         })}
       </div>
-    );
+    )
   }
 }
+
+const Card = styled.a`
+max-width: 350px;
+width: 100%;
+display: inline-block;
+border-radius: 3px;
+text-decoration: none;
+color: #000;
+margin: 0 10px 15px;
+box-shadow: 7px 7px 50px -10px rgba(0, 0, 0, .5);
+`;
