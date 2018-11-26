bcc4a930946690acf7b59a4de761c7edc82e839b
Nico <nico@Nicos-MacBook-Pro.local>
2018-11-26 17:37:19 +0200

    add Title page

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import Title from './Title';
-import Image from './Image';
+import React, { Component } from "react";
+import Title from "./Title";
+import Image from "./Image";
 export default class MealCard extends Component {
-    render(){
-        return(
-            <div>
-                {this.props.meals.map((item, index) => {
-                    return (
-                        <a
-                        key={index}
-                        href={item.strSource}>
-                        <Image source={item.strMealThumb} text={item.strMeal} />
-                        <Title title={item.strMeal} />
-                        </a>
-                    )
-                })}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {this.props.meals.map((item, index) => {
+          return (
+            <a key={index} href={item.strSource}>
+              <Image source={item.strMealThumb} text={item.strMeal} />
+              <Title title={item.strMeal} />
+            </a>
+          );
+        })}
+      </div>
+    );
+  }
 }
