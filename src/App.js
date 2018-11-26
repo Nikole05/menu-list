f01bd0caea60092a76a555716498b82f7b4364dd
Nico <nico@Nicos-MacBook-Pro.local>
2018-11-26 16:43:46 +0200

    using axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      meal: ""
+    }
+  }
+  componentDidMount(){
+    const URL = 'https://www.themealdb.com/api/json/v1/1/latest.php';
+    axios.get(URL)
+    .then(res => {
+      const meal = res.data.meals;
+      if(typeof meal === 'object'){
+        this.setState({ meal });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
   render() {
     return (
       <div className="App">
