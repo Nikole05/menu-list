172b4ccae3de196505987ea4f4057f6bd395c169
Nico <nico@Nicos-MacBook-Pro.local>
2018-11-26 19:11:35 +0200

    using the styled-components in app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
 import axios from "axios";
+import styled from 'styled-components';
 import MealCard from "./components/MealCard";
 import "./App.css";
 
-class App extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       meal: {}
-    };
+    }
   }
+
   componentDidMount() {
     const URL = "https://www.themealdb.com/api/json/v1/1/latest.php";
-    axios
-      .get(URL)
+
+    axios.get(URL)
       .then(res => {
         const meal = res.data.meals;
         if (typeof meal === "object") {
@@ -23,13 +26,20 @@ class App extends Component {
       .catch(error => {
         console.log(error);
       });
+
   }
+
   render() {
     var data = this.state.meal;
     return (
-      <div className="App">{data.length > 0 && <MealCard meals={data} />}</div>
+      <AppContainer className="App">
+      {data.length > 0 && <MealCard meals={data} />}
+      </AppContainer>
     );
   }
 }
 
-export default App;
+const AppContainer = styled.div`
+background: #fff;
+padding: 2em;
+`
